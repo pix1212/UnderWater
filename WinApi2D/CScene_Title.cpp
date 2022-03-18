0bbee14eb499ff8eabc913dc88ee4530bed84233
kimsanta <97747770+kimsanta@users.noreply.github.com>
2022-03-18 16:44:44 +0900

    초기 커밋.

---
+++ WinApi2D/CScene_Title.cpp
@@ -0,0 +1,23 @@
+#include "framework.h"
+#include "CScene_Title.h"
+
+CScene_Title::CScene_Title()
+{
+}
+
+CScene_Title::~CScene_Title()
+{
+}
+
+void CScene_Title::Enter()
+{
+	//TODO:
+	//1. 로고 출력용 오브젝트 출력
+	//2. 시작버튼
+	//3. 종료버튼
+}
+
+void CScene_Title::Exit()
+{
+}
+
